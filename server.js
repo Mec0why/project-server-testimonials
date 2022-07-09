1f0eeffee4f002fafdaf0f8fb55d8267f67ebb44
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-09 18:27:54 +0200

    Add database

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const path = require('path');
-var cors = require('cors');
+const cors = require('cors');
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  {
+    id: 2,
+    author: 'Amanda Doe',
+    text: 'They really know how to make you happy.',
+  },
+  { id: 3, author: 'Dustin Henderson', text: 'Never ending storyyy...' },
+  { id: 4, author: 'Lucas Sinclair', text: 'Kate Bush will help You' },
+  { id: 5, author: 'Mike Willer', text: 'I count to eleven' },
+  { id: 6, author: 'Will Byers', text: 'Lost in time and upside down' },
+  {
+    id: 7,
+    author: 'Jim Hopper',
+    text: 'Mornings are for coffee and contemplation',
+  },
+];
 
 const app = express();
 app.use(
