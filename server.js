097baba77dd34105e8a780cc97cf9a3b722121e0
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-09 18:14:34 +0200

    Add server.js and implement express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use((req, res) => {
+    res.status(404).send('404 You shall not pass!');
+  });
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
