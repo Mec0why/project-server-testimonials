db91a39c68ecc03acccaa5b83103ba64c1127eb2
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-09 18:22:31 +0200

    Add cors middleware to control sharing settings of the API server

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 
 const app = express();
+app.use(
+  cors({
+    origin: 'https://kodilla.com', //origin sets domains that we approve
+    methods: 'GET,POST', //we allow only GET and POST methods
+  })
+);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use((req, res) => {
-    res.status(404).send('404 You shall not pass!');
-  });
+  res.status(404).send('404 You shall not pass!');
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
