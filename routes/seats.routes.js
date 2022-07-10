9aa4f3c22d240d563ae267d71a71547fbe4921cc
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-10 16:12:12 +0200

    Move seats endpoints to routes/seats.routes.js

---
+++ routes/seats.routes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+const shortid = require('shortid');
+
+router.route('/seats').get((req, res) => {
+  res.json(db.seats);
+});
+
+router.route('/seats/:id').get((req, res) => {
+  res.json(db.seats[req.params.id - 1]);
+});
+
+router.route('/seats').post((req, res) => {
+  const { day, seat, client, email } = req.body;
+
+  const seatObj = {
+    id: shortid.generate(),
+    day: day,
+    seat: seat,
+    client: client,
+    email: email,
+  };
+
+  db.seats.push(seatObj);
+
+  res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').put((req, res) => {
+  const id = parseInt(req.params.id);
+  const { day, seat, client, email } = req.body;
+
+  db.seats.map((seatObj, i) =>
+    seatObj.id === id
+      ? (db.seats[i] = {
+          ...seatObj,
+          day: day,
+          seat: seat,
+          client: client,
+          email: email,
+        })
+      : seatObj
+  );
+
+  res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').delete((req, res) => {
+  const id = parseInt(req.params.id);
+
+  const filteredDb = db.seats.filter((seatObj) =>
+    seatObj.id === id ? false : true
+  );
+
+  console.log(filteredDb);
+  res.json({ message: 'OK' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,84 +1,31 @@
-const db = require('./db.js');
+// initialize server
 const express = require('express');
-
 const app = express();
 
-const testimonialsRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-
+// import additional packages
 const path = require('path');
 const cors = require('cors');
-const shortid = require('shortid');
 
+// import endpoints
+const testimonialsRoutes = require('./routes/testimonials.routes'); // import testimonials routes
+const concertsRoutes = require('./routes/concerts.routes'); // import concerts routes
+const seatsRoutes = require('./routes/seats.routes'); // import seats routes
+
+// use additional packages
 app.use(
   cors({
     origin: 'https://kodilla.com', //origin sets domains that we approve
     methods: 'GET, POST', //we allow only GET and POST methods
   })
 );
-
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
+// use endpoints
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
 app.use('/api', concertsRoutes); // add concerts routes to server
-
-app.get('/seats', (req, res) => {
-  res.json(db.seats);
-});
-
-app.get('/seats/:id', (req, res) => {
-  res.json(db.seats[req.params.id - 1]);
-});
-
-app.post('/seats', (req, res) => {
-  const { day, seat, client, email } = req.body;
-
-  const seatObj = {
-    id: shortid.generate(),
-    day: day,
-    seat: seat,
-    client: client,
-    email: email,
-  };
-
-  db.seats.push(seatObj);
-
-  res.json({ message: 'OK' });
-});
-
-app.put('/seats/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const { day, seat, client, email } = req.body;
-
-  db.seats.map((seatObj, i) =>
-    seatObj.id === id
-      ? (db.seats[i] = {
-          ...seatObj,
-          day: day,
-          seat: seat,
-          client: client,
-          email: email,
-        })
-      : seatObj
-  );
-
-  res.json({ message: 'OK' });
-});
-
-app.delete('/seats/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const filteredDb = db.seats.filter((seatObj) =>
-    seatObj.id === id ? false : true
-  );
-
-  console.log(filteredDb);
-  res.json({ message: 'OK' });
-});
+app.use('/api', seatsRoutes); // add seats routes to server
 
 app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
