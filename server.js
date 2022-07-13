405237f8dac30db5d0f6bae9aaa1d2fd9921e49f
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-13 18:44:35 +0200

    Add code in server.js to return Reactapp on No-match

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
 });
