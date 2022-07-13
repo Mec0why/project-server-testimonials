d8f640096d64b06502b3971375ea9ec5b799e3a6
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-13 18:53:01 +0200

    Update server Port to work on Heroku

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
