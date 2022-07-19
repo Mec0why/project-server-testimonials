9084c3a399e71d70ccd0bc491487d1609ebb36c7
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-19 18:58:19 +0200

    Add and initialize socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // initialize server
 const express = require('express');
 const app = express();
+const io = require('socket.io'),
 
 // import additional packages
 const path = require('path');
@@ -43,6 +44,17 @@ app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const socket = io(server);
+
+socket.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  
+
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
 });
