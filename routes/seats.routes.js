876f30a4cd732704f1faec0d51ac89cb61d4df11
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-10 13:51:51 +0200

    Update PUT/seats - Add Booked Validation and response with new Object

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -52,18 +52,18 @@ router.post('/seats', async (req, res) => {
   try {
     const { day, seat, client, email } = req.body;
 
-    const eventCheck = await Concert.findOne({ day: day });
+    const eventMatch = await Concert.findOne({ day: day });
 
-    if (!eventCheck) {
-      res.status(409).json({ message: 'There are no events on this date...' });
+    if (!eventMatch) {
+      res.status(404).json({ message: 'There are no events on this date...' });
     } else {
       const seatCheck = await Seat.findOne({
-        $and: [{ seat: seat }, { event: eventCheck._id }],
+        $and: [{ seat: seat }, { event: eventMatch._id }],
       });
 
       if (!seatCheck) {
         const newSeat = new Seat({
-          event: eventCheck,
+          event: eventMatch,
           seat: seat,
           client: client,
           email: email,
@@ -101,6 +101,48 @@ router.post('/seats', async (req, res) => {
 
 //   res.json({ message: 'OK' });
 // });
+
+router.put('/seats/:id', async (req, res) => {
+  try {
+    const { day, seat, client, email } = req.body;
+
+    const eventMatch = await Concert.findOne({ day: day });
+
+    if (!eventMatch) {
+      res.status(404).json({ message: 'There are no events on this date...' });
+    } else {
+      const seatCheck = await Seat.findOne({
+        $and: [{ seat: seat }, { event: eventMatch._id }],
+      });
+
+      if (seatCheck) {
+        res.status(404).json({ message: 'Seat Already Booked...' });
+      } else {
+        const seatMatch = await Seat.findById(req.params.id);
+
+        if (seatMatch) {
+          const updatedSeat = await Seat.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+              $set: {
+                event: eventMatch,
+                seat: seat,
+                client: client,
+                email: email,
+              },
+            },
+            { new: true }
+          );
+          res.json(updatedSeat);
+        } else {
+          res.status(404).json({ message: 'Seat Not found...' });
+        }
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
 
 // router.route('/seats/:id').delete((req, res) => {
 //   const id = parseInt(req.params.id);
