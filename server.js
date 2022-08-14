174da9cd0eade8d4be9808abee289d7854f28898
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-14 23:43:48 +0200

    Prepare Testing environment and Execute GET Concerts tests

--- server.js
+++ server.js
@@ -45,19 +45,32 @@ app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
 });
 
-// connects our backend code with the database
-mongoose.connect(
-  'mongodb+srv://mikolaj-renke:47T4zs5BstwgApT@newwavefestival.esofyuj.mongodb.net/NewWaveDB'
-);
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production')
+  dbUri =
+    'mongodb+srv://mikolaj-renke:47T4zs5BstwgApT@newwavefestival.esofyuj.mongodb.net/NewWaveDB';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBtest';
+else
+  dbUri =
+    'mongodb+srv://mikolaj-renke:47T4zs5BstwgApT@newwavefestival.esofyuj.mongodb.net/NewWaveDB';
+
+// connects our backend code with the database based on Node environment
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('Connected to the database');
+  if (NODE_ENV !== 'test') {
+    console.log('Connected to the database');
+  }
 });
 db.on('error', (err) => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+  if (NODE_ENV !== 'test') {
+    console.log('Server is running on port: 8000');
+  }
 });
 
 const io = socket(server);
@@ -71,3 +84,5 @@ io.on('connection', (socket) => {
     console.log('Oh, socket ' + socket.id + ' has left');
   });
 });
+
+module.exports = server;
